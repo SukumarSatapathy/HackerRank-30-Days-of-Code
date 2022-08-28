378b59bb1408fde045f65a9d7944339b87ade439
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-08-28 22:51:50 +0530

    Question Description
    Objective
    Today, we're delving into Inheritance. Check out the attached tutorial for learning materials and an instructional video.
    
    Task
    You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed
    code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the
    properties of Person.
    
    Complete the Student class by writing the following:
    A Student class constructor, which has 4 parameters:
    1. A string, first Name.
    2. A string, last Name.
    3. An integer, idNumber.
    4. An integer array (or vector) of test scores, scores.
    
    A char calculate() method that calculates a Student object's average and returns the grade character representative of their
    calculated average:

---
+++ Day 12: Inheritance.py
@@ -0,0 +1,43 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self, firstName, lastName, idNumber, scores):
+        Person.__init__(self, firstName, lastName, idNumber)
+        self.scores = scores
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        a = sum(self.scores)/len(self.scores)
+        gradeBook = {90:'O', 80:'E', 70:'A', 55:'P', 40:'D', 0:'T'}
+        for value in gradeBook:
+            if a >= value:
+                return gradeBook[value]
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
